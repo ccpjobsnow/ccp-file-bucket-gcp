0aec705791f81a52021c0d510745d3e32241c1c0
onias <onias@noxxonsat.com.br>
2023-07-22 20:18:45 -0300

    .

--- src/main/java/com/ccp/implementations/file/bucket/FileBucket.java
+++ src/main/java/com/ccp/implementations/file/bucket/FileBucket.java
@@ -2,7 +2,7 @@ package com.ccp.implementations.file.bucket;
 
 import com.ccp.dependency.injection.CcpModuleExporter;
 
-public class Bucket implements CcpModuleExporter {
+public class FileBucket implements CcpModuleExporter {
 
 	@Override
 	public Object export() {
