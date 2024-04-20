4fb33140dce75e9e0222b4779d5aecb12d02a1c9
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2024-04-20 19:41:27 -0300

    .

--- src/main/java/com/ccp/implementations/file/bucket/gcp/CcpGcpFileBucket.java
+++ src/main/java/com/ccp/implementations/file/bucket/gcp/CcpGcpFileBucket.java
@@ -1,11 +1,11 @@
 package com.ccp.implementations.file.bucket.gcp;
 
 import com.ccp.dependency.injection.CcpInstanceProvider;
+import com.ccp.especifications.file.bucket.CcpFileBucket;
 
-public class CcpGcpFileBucket implements CcpInstanceProvider {
+public class CcpGcpFileBucket implements CcpInstanceProvider<CcpFileBucket> {
 
-	
-	public Object getInstance() {
+	public CcpFileBucket getInstance() {
 		return new GcpFileBucket();
 	}
 
